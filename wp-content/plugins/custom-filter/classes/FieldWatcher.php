5bf8f849f01bfdaeca9eb53f411da796879be415
dmitry.k <dmitry.k@nimax.agency>
2023-03-24 18:44:19 +0300

    field watcher for generate select with types

--- wp-content/plugins/custom-filter/classes/FieldWatcher.php
+++ wp-content/plugins/custom-filter/classes/FieldWatcher.php
@@ -3,7 +3,11 @@
 //todo добавить сопоставления допустимых типов полей с типом фильтра (селект, чекбокс, диапазон(только для числовых) )
 class FieldWatcher
 {
-    public static $allowedFieldTypes = ['number', 'text', 'range', 'select', 'checkbox', 'radio', 'true_false'];
+    private static $allowedFieldTypes = ['number', 'text', 'range', 'select', 'checkbox', 'radio', 'true_false'];
+    const CHECKBOX_TYPE = ['checkbox' => 'Чекбокс'];
+    const RADIO_TYPE = ['radio' => 'Радио-кнопка'];
+    const SELECT_TYPE = ['select' => 'Селект'];
+    const RANGE_TYPE = ['range' => 'Диапазон'];
 
     /**
      * @param $fieldType string
@@ -16,5 +20,46 @@ class FieldWatcher
         }
 
         return false;
+    }
+
+    public static function getAllowedFilterTypesByFieldType($fieldType)
+    {
+        $filterTypes = [];
+        switch ($fieldType){
+            case 'number':
+                $filterTypes = self::CHECKBOX_TYPE + self::RADIO_TYPE + self::SELECT_TYPE + self::RANGE_TYPE;
+                break;
+            case 'text':
+            case 'select':
+            case 'checkbox':
+            case 'radio':
+            case 'true_false':
+                $filterTypes = self::CHECKBOX_TYPE + self::RADIO_TYPE + self::SELECT_TYPE;
+                break;
+            case 'range':
+                $filterTypes = self::RANGE_TYPE;
+            default:
+                break;
+        }
+        return $filterTypes;
+    }
+
+    /**
+     * @param string $fieldName - название поля для name селекта (ключ поля в acf)
+     * @param string $fieldType - тип поля в acf (number', 'text' и т.д.)
+     * @param string $selectedType - выбранный тип фильтра у сохраненного поля
+     */
+    public static function printFilterTypesSelect($fieldName, $fieldType, $selectedType = '')
+    {
+        $filterTypes = self::getAllowedFilterTypesByFieldType($fieldType)
+        //todo переделать в кастомный селект
+        ?>
+        <select name="<?=$fieldName?>">
+            <option <?=$selectedType ? '' : 'selected'?> disabled>Выбрать</option>
+            <? foreach ($filterTypes as $k => $option) { ?>
+                <option <?=$selectedType == $k ? 'selected' : ''?> value="<?=$k?>"><?=$option?></option>
+            <? } ?>
+        </select>
+        <?
     }
 }
